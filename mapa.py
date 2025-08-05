46be377f60d8131b839573e5ff80eea400c9d862
safiracode <safira.moraes.dev@gmail.com>
2025-08-05 12:50:57 -0300

    Melhora na criação do mapa
    Os mapas gerados estavam tendo partes desconexas e rotas unicas, o que dificutaria fugir dos seguranças

--- mapa.py
+++ mapa.py
@@ -1,4 +1,4 @@
-# Arquivo: mapa.py (Versão Final com Variedade e Espaçamento)
+# Arquivo: mapa.py (Versão Final Polida com Bordas Limpas)
 import random
 import constants
 
@@ -12,68 +12,95 @@ PISO = 0
 
 def gerar_mapa_aleatorio(largura, altura):
     """
-    Gera um mapa com T, L e linhas, garantindo espaçamento e sem formar quadrados.
+    Gera um labirinto complexo com bordas limpas e espaçadas.
     """
-    # 1. Começa com uma grade cheia de PISO
-    grade = [[PISO for _ in range(largura)] for _ in range(altura)]
+    # 1. Começa com uma grade cheia de PAREDES
+    grade = [[PAREDE for _ in range(largura)] for _ in range(altura)]
 
-    # 2. Constrói a "muralha" ao redor do mapa
+    # 2. Cava os corredores
+    pilha = []
+    x_inicio = random.choice(range(1, largura, 2))
+    y_inicio = random.choice(range(1, altura, 2))
+    grade[y_inicio][x_inicio] = PISO
+    pilha.append((x_inicio, y_inicio))
+
+    while pilha:
+        x, y = pilha[-1]
+        vizinhos = []
+        if y > 1 and grade[y - 2][x] == PAREDE: vizinhos.append((x, y - 2, x, y - 1))
+        if y < altura - 2 and grade[y + 2][x] == PAREDE: vizinhos.append((x, y + 2, x, y + 1))
+        if x > 1 and grade[y][x - 2] == PAREDE: vizinhos.append((x - 2, y, x - 1, y))
+        if x < largura - 2 and grade[y][x + 2] == PAREDE: vizinhos.append((x + 2, y, x + 1, y))
+        
+        if vizinhos:
+            x_novo, y_novo, x_meio, y_meio = random.choice(vizinhos)
+            grade[y_meio][x_meio] = PISO
+            grade[y_novo][x_novo] = PISO
+            pilha.append((x_novo, y_novo))
+        else:
+            pilha.pop()
+
+    # 3. Remove os becos sem saída
+    becos_sem_saida = []
+    for y in range(1, altura - 1):
+        for x in range(1, largura - 1):
+            if grade[y][x] == PISO:
+                vizinhos_parede = 0
+                if grade[y-1][x] == PAREDE: vizinhos_parede += 1
+                if grade[y+1][x] == PAREDE: vizinhos_parede += 1
+                if grade[y][x-1] == PAREDE: vizinhos_parede += 1
+                if grade[y][x+1] == PAREDE: vizinhos_parede += 1
+                if vizinhos_parede >= 3: becos_sem_saida.append((x, y))
+
+    for x, y in becos_sem_saida:
+        paredes_adjacentes = []
+        if y > 1 and grade[y-1][x] == PAREDE: paredes_adjacentes.append((x, y-1))
+        if y < altura - 2 and grade[y+1][x] == PAREDE: paredes_adjacentes.append((x, y+1))
+        if x > 1 and grade[y][x-1] == PAREDE: paredes_adjacentes.append((x-1, y))
+        if x < largura - 2 and grade[y][x+1] == PAREDE: paredes_adjacentes.append((x+1, y))
+        if paredes_adjacentes:
+            px, py = random.choice(paredes_adjacentes)
+            grade[py][px] = PISO
+            
+    # 4. Cria loops e atalhos extras
+    loops_extras_a_criar = 50 
+    for _ in range(loops_extras_a_criar):
+        x = random.randrange(1, largura - 1)
+        y = random.randrange(1, altura - 1)
+        if grade[y][x] == PAREDE:
+            if (grade[y-1][x] == PISO and grade[y+1][x] == PISO):
+                grade[y][x] = PISO
+            elif (grade[y][x-1] == PISO and grade[y][x+1] == PISO):
+                grade[y][x] = PISO
+
+    # --- NOVO PASSO: O "PAISAGISTA DE BORDAS" ---
+    # Limpa as paredes que correm paralelas à muralha externa.
+    chance_de_limpar_borda = 0.80 # 80% de chance de remover uma parede paralela
+
+    # Limpa borda de cima (linha y=1)
+    for x in range(1, largura - 1):
+        if grade[1][x] == PAREDE and (grade[1][x-1] == PAREDE or grade[1][x+1] == PAREDE):
+            if random.random() < chance_de_limpar_borda: grade[1][x] = PISO
+    # Limpa borda de baixo (linha y = altura-2)
+    for x in range(1, largura - 1):
+        if grade[altura-2][x] == PAREDE and (grade[altura-2][x-1] == PAREDE or grade[altura-2][x+1] == PAREDE):
+            if random.random() < chance_de_limpar_borda: grade[altura-2][x] = PISO
+    # Limpa borda da esquerda (coluna x=1)
+    for y in range(1, altura - 1):
+        if grade[y][1] == PAREDE and (grade[y-1][1] == PAREDE or grade[y+1][1] == PAREDE):
+            if random.random() < chance_de_limpar_borda: grade[y][1] = PISO
+    # Limpa borda da direita (coluna x = largura-2)
+    for y in range(1, altura - 1):
+        if grade[y][largura-2] == PAREDE and (grade[y-1][largura-2] == PAREDE or grade[y+1][largura-2] == PAREDE):
+            if random.random() < chance_de_limpar_borda: grade[y][largura-2] = PISO
+
+    # 6. PASSO FINAL: A MURALHA FINAL
+    # Este passo garante que a moldura do mapa esteja sempre intacta.
     for x in range(largura):
         grade[0][x] = PAREDE
         grade[altura - 1][x] = PAREDE
     for y in range(altura):
         grade[y][0] = PAREDE
         grade[y][largura - 1] = PAREDE
-        
-    # --- MODELOS DE OBSTÁCULOS FINOS ESTÃO DE VOLTA ---
-    obstaculos_modelos = [
-        # Linhas
-        [[1, 1, 1]], [[1, 1, 1, 1]],
-        [[1], [1], [1]], [[1], [1], [1], [1]],
-        # Cantos (L) - Todas as 4 rotações
-        [[1, 1], [1, 0]], [[1, 1], [0, 1]],
-        [[1, 0], [1, 1]], [[0, 1], [1, 1]],
-        # Formas de T - Todas as 4 rotações
-        [[1, 1, 1], [0, 1, 0]], [[0, 1, 0], [1, 1, 1]],
-        [[1, 0], [1, 1], [1, 0]], [[0, 1], [1, 1], [0, 1]]
-    ]
-
-    # Aumentado para um mapa bem mais cheio
-    tentativas_de_colocacao = 100
-    gerar_simetrico = True
-
-    # 3. Tenta colocar os obstáculos variados
-    for _ in range(tentativas_de_colocacao):
-        obstaculo = random.choice(obstaculos_modelos)
-        obs_altura = len(obstaculo)
-        obs_largura = len(obstaculo[0])
-
-        pos_x = random.randrange(2, largura - obs_largura - 2)
-        pos_y = random.randrange(2, altura - obs_altura - 2)
-
-        # A "Regra de Ouro": Verifica a área de segurança ao redor do obstáculo
-        area_ok = True
-        for y_verif in range(pos_y - 1, pos_y + obs_altura + 1):
-            for x_verif in range(pos_x - 1, pos_x + obs_largura + 1):
-                if grade[y_verif][x_verif] == PAREDE:
-                    area_ok = False; break
-            if not area_ok: break
-        
-        # Se a área estiver limpa, coloca o obstáculo
-        if area_ok:
-            for y_obs in range(obs_altura):
-                for x_obs in range(obs_largura):
-                    if obstaculo[y_obs][x_obs] == 1:
-                        grade[pos_y + y_obs][pos_x + x_obs] = PAREDE
-                        if gerar_simetrico:
-                            x_espelhado = largura - 1 - (pos_x + x_obs)
-                            grade[pos_y + y_obs][x_espelhado] = PAREDE
-
-    # 4. "Fiscal Anti-Quadrados" como garantia final
-    for y in range(altura - 1):
-        for x in range(largura - 1):
-            if (grade[y][x] == PAREDE and grade[y+1][x] == PAREDE and
-                grade[y][x+1] == PAREDE and grade[y+1][x+1] == PAREDE):
-                grade[y + random.randint(0, 1)][x + random.randint(0, 1)] = PISO
-
+                
     return grade
